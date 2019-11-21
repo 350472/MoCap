365b27f1c3f46e6f152183f7ae401d8eea05b2f3
LoggerPro <loggerpro@beaverton.k12.or.us>
2019-11-21 11:35:05 -0800

    MoCap

---
+++ VideoCapV0.py
@@ -0,0 +1,21 @@
+"""
+11/19/19
+Alex Fryer
+Code to Capture Joint Angle
+"""
+
+#import libraries
+
+#Request min/max angles
+
+#Capture image from camera
+
+#Detect edges in video
+
+#Create window
+
+#Display images in window
+
+#Wait for button press
+
+#Destroy windows/save
