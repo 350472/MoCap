8598cbc527c2e097e621ae60a630e41b474d5de6
LoggerPro <loggerpro@beaverton.k12.or.us>
2019-11-21 12:26:58 -0800

    Commit Message

--- VideoCapV0.py
+++ VideoCapV0.py
@@ -5,17 +5,24 @@ Code to Capture Joint Angle
 """
 
 #import libraries
+import cv2
+key = 0
 
 #Request min/max angles
+cap = cv2.VideoCapture("obrism.gif")
 
+while (key != 27):
+    
 #Capture image from camera
-
-#Detect edges in video
-
+    ret, frame = cap.read()
 #Create window
+    cv2.namedWindow("Vid",cv2.WINDOW_NORMAL)
 
 #Display images in window
+    cv2.imshow("Vid",frame)
 
 #Wait for button press
-
+    key = cv2.waitKey(1)
 #Destroy windows/save
+    if key == 27:
+        cv2.destroyAllWindows()
